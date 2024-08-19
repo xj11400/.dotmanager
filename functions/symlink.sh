8254222aa275761186689fc3b30807fe5cacfaa0
XJ Hsu <xj11400@gmail.com>
2024-08-19 21:37:13 +0800

    <modify>(function/symlink.sh): new functions

--- functions/symlink.sh
+++ functions/symlink.sh
@@ -4,7 +4,6 @@
 # This file contains utility functions for managing symbolic links.
 # ====================================================================================================
 
-
 function list_directories() {
     local dir="$1"
     local exclude_items=("\.git") # TODO: add more exclude items
@@ -111,60 +110,24 @@ function list_files_and_directories() {
     echo "${sorted_root_dirs[*]}"
 }
 
-function symlink() {
-    local src=""
-    local target=""
-    local _opt_files=false
-    local _opt_resymlink=false
+function _symlink() {
+    local src_dir=$1
+    local target_dir=$2
+    local opt_files=$3
 
-    # Parse parameters
-    for arg in "$@"; do
-        case $arg in
-        --target=*)
-            target="${arg#*=}"
-            ;;
-        --files)
-            _opt_files=true
-            ;;
-        --resymlink)
-            _opt_resymlink=true
-            ;;
-        *)
-            src="$arg"
-            ;;
-        esac
-    done
-
-    # Resolve realpath
-    src=$(realpath "$src")
-    target=$(realpath "$target")
-    log_debug ""
-    log_debug "              src: $src"
-    log_debug "              target: $target"
-    # Check if src and target are set
-    if [ -z "$src" ] || [ -z "$target" ]; then
-        msg_error "Error: Both source and target must be specified."
-        return 1
-    fi
-
-    # Get the list of items to symlink
-    local _symlink_items
-    if [ "$_opt_files" = true ]; then
-        _symlink_items=$(list_files_recursive "$src")
+    local _symlink_items=()
+    if [ "$opt_files" = true ]; then
+        _symlink_items+=($(list_files_recursive "$src_dir"))
     else
-        _symlink_items=$(list_files_and_directories "$src")
+        _symlink_items+=($(list_files_and_directories "$src_dir"))
     fi
-
-    # Create target directory if it doesn't exist
-    [ ! -d "$target" ] && mkdir -p "$target"
 
     # Check if the target item already exists
     local _target_item
     local _items_to_symlink=()
-    while IFS= read -r _item; do
+    for _item in "${_symlink_items[@]}"; do
         log_info "symlink item: $_item"
-        _target_item="$target/$_item"
-
+        _target_item="$target_dir/$_item"
         if [ -e "$_target_item" ] || [ -L "$_target_item" ]; then
             log_info "Warning: '$_target_item' already exists."
             # Check if the target item is not a symlink
@@ -186,22 +149,102 @@ function symlink() {
         else
             _items_to_symlink+=("$_item")
         fi
-    done <<< "$_symlink_items"
+    done
 
     # Perform symlink
     local _src_item
     local _target_parent
     for _item in "${_items_to_symlink[@]}"; do
-        log_debug " --------- $_item"
-        _src_item="$src/$_item"
-        _target_item="$target/$_item"
+        log_debug "     --- $_item"
+        _src_item="$src_dir/$_item"
+        _target_item="$target_dir/$_item"
 
         # Create parent directory if it doesn't exist
         _target_parent=$(dirname "$_target_item")
         [ ! -d "$_target_parent" ] && mkdir -p "$_target_parent"
 
         # Create the symlink
-        log_debug "ln -s $_src_item $_target_item"
+        log_debug "      > ln -s $_src_item $_target_item"
         ln -s "${_src_item}" "${_target_item}"
+    done
+}
+
+function symlink() {
+    local src_dir=()
+    local target=""
+    # parse options
+    local _opt_files=false
+    local _opt_resymlink=false
+
+    # Parse parameters
+    for arg in "$@"; do
+        case $arg in
+        --target=*)
+            target="${arg#*=}"
+            ;;
+        --source=*)
+            src_dir+=("${arg#*=}")
+            ;;
+        --files)
+            _opt_files=true
+            ;;
+        --resymlink)
+            _opt_resymlink=true
+            ;;
+        --help | -h)
+            echo "Create symlinks from specific directories to target path."
+            echo ""
+            echo "Usage: $0 [OPTIONS] <directory>"
+            echo ""
+            echo "Options:"
+            echo "  --files               Create a symlink for each file in the directory"
+            echo "  --resymlink           Resymlink existing symlinks or create new symlinks"
+            echo "  --help, -h            Display this help message"
+            echo ""
+            echo "Specify options:"
+            echo "  --source=<path>  Specify a custom dotfiles directory"
+            echo "  --target=<path>  Specify a custom target directory"
+            echo ""
+            exit 0
+            ;;
+        *)
+            src_dir+=("$current_dir/$arg")
+            ;;
+        esac
+    done
+
+    # parse ~ to $HOME
+    for i in "${!src_dir[@]}"; do
+        src_dir[$i]="${src_dir[$i]/#\~/$HOME}"
+    done
+    target="${target/#\~/$HOME}"
+
+    # Resolve realpath
+    for i in "${!src_dir[@]}"; do
+        src_dir[$i]=$(realpath "${src_dir[$i]}")
+        if [ -z "${src_dir[$i]}" ]; then
+            msg_error "Error: Source path '${src_dir[$i]}' is not valid."
+            return 1
+        fi
+    done
+
+    target_dir=$(realpath "$target" 2>&- || echo "")
+    if [ -z "$target_dir" ]; then
+        if [ -d "$(realpath "$(dirname "$target")")" ]; then
+            log_info "Creating target directory: $target"
+            mkdir -p "$target"
+            target_dir=$(realpath "$target")
+        else
+            msg_error "Error: Target must be specified."
+            return 1
+        fi
+    fi
+
+    log_info " [symlink] src: ${src_dir[@]}"
+    log_info " [symlink] target: $target_dir"
+
+    # Get the list of items to symlink
+    for _src_dir in "${src_dir[@]}"; do
+        _symlink $_src_dir $target_dir $_opt_files
     done
 }
